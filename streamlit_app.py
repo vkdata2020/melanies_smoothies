0b6c562b3ec82ddc1274bdd260eacd02c73ff6f8
vkdata2020 <78692026+vkdata2020@users.noreply.github.com>
2025-04-19 14:01:38 +0530

    Update streamlit_app.py
    rename df

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)        
-        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)        
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.text(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
