f78458d92bb66f1485e26939ca92416cd3068a92
vkdata2020 <78692026+vkdata2020@users.noreply.github.com>
2025-04-19 11:38:00 +0530

    Update streamlit_app.py
    move smoothie_fruit response inside for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")        
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.text(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -37,6 +39,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
