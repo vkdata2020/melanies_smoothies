46dae78d6e746afb7e670eab203dbed8bdbb8680
vkdata2020 <78692026+vkdata2020@users.noreply.github.com>
2025-04-19 13:33:23 +0530

    Update streamlit_app.py
    search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 #pd_df = my_dataframe.to_pandas()
